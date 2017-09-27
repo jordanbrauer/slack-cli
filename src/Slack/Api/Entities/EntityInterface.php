d9cb773dacc882dc6a1e1ad6fff46489add7adbe
Jordan <jbrauer.inc@gmail.com>
2017-09-27 00:00:30 -0500

    Update: syntax and formatting

--- src/Slack/Api/Entities/EntityInterface.php
+++ src/Slack/Api/Entities/EntityInterface.php
@@ -1,38 +1,48 @@
 <?php
 
-/**
- * @author Jordan Brauer <info@jordanbrauer.ca>
- * @version 0.0.1
- */
-
 declare (strict_types = 1);
 
 namespace Slack\Api\Entities;
 
-use Symfony\Component\Console\{
-  Output\OutputInterface,
-  Style\SymfonyStyle
-};
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
+/**
+ * The interface for a Slack API entity. An entity could be
+ * a channel, message, user, or any object with properties.
+ *
+ * @author Jordan Brauer <info@jordanbrauer.ca>
+ * @version 0.0.1
+ */
 interface EntityInterface
 {
   /**
-   * Must output a json string
+   * The implementing class must be able to output a JSON string
+   *
+   * @param SymfonyStyle $io An input/ouput SymfonyStyle object
+   * @param string|array $json The JSON data structure to format an print out to the console
+   * @return null
    */
   public static function getJsonOutput (SymfonyStyle $io, $json);
 
   /**
-   * Method for building table headers from the entity
+   * The implementing class must be able to output a formatted table string using
+   * the getTableHeaders and getTableRows methods
    */
-  public static function getTableHeaders ();
+  public static function getTableOutput (OutputInterface $output, array $entities = array(), array $parameters = array());
+
+  /**
+   * The implementing class must have a helper method for building table column
+   * headers from the entity
+   *
+   * @return array
+   */
+  public static function getTableHeaders (): array;
 
   /**
    * Method for building the table rows, 1 per entity
+   *
+   * @return array
    */
-  public static function getTableRows (array $entities = array(), array $parameters = array());
-
-  /**
-   * Must output a formatted table string using the getTableHeaders and getTableRows methods
-   */
-  public static function getTableOutput (OutputInterface $output, array $entities = array(), array $parameters = array());
+  public static function getTableRows (array $entities = array(), array $parameters = array()): array;
 }
